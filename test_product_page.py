6417c91a37045d7ce0884955362ad1affb320ae0
olhasemenova <olha.sergeevna87@gmail.com>
2022-10-17 09:33:20 +0300

    add another version of test parametrization

--- test_product_page.py
+++ test_product_page.py
@@ -36,7 +36,12 @@ def test_guest_can_add_product_to_basket_parametrized(browser, link):
     page.solve_quiz_and_get_code()
     page.should_be_message_about_product_added_to_basket()
     page.message_price_should_be_equal_to_basket_price()
-    
+#@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+#def test_guest_can_add_product_to_basket_with_promo(browser, promo_offer):
+    #link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    #page = ProductPage(browser, link)
+    #page.open()
+    #page.add_product_to_basket()    
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
